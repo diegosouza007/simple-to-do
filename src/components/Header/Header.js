2ef095fcdaee7b1dc6ff590b6aa3a3f87df451d6
diegosouza007 <dsouza@yahoo.com>
2022-02-08 23:12:40 -0300

    date function added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Theme from '../Theme/Theme'
 import './Header.css';
 
+
 function Header(props) {
+
+    const [date, setDate] = useState('');
+
+    useEffect(()=>{
+        const names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+        const d = new Date();
+        const today = `${names[d.getDay()]} ${d.getDate()}, ${d.getFullYear()}`
+        
+        setDate(today);
+    }, []);
+
     return (
         <div className="header">
             <Theme onHandleTheme={props.onHandleTheme} theme={props.theme}/>
-            <h3>Tuesday 08, 2022</h3>
+            <h3>{date}</h3>
         </div>
     )
 }
